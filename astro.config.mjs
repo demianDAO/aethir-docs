92d1a92e9c56fc19a64c5ae3d4659e7c24ffe52b
chuhemiao <535101602@qq.com>
2023-08-01 22:50:56 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,35 @@
-import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Aethir 中文指南',
+      social: {
+        github: 'https://github.com/demianDAO/aethir-docs'
+      },
+      sidebar: [
+        {
+          label: '新手指南',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: '什么是Aethir', link: '/guides/what-is-aethir' },
+            { label: '埃瑟尔的进度更新#1', link: '/guides/progress-update-1' },
+            {
+              label: '埃瑟尔完成 1.5 亿美元的 Pre-A 轮融资',
+              link: '/guides/aethir-closes-150-m'
+            }
+          ]
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' }
+        }
+      ]
+    })
+  ],
 
-	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } }
 });
